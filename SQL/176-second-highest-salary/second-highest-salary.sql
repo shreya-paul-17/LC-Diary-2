31a30c01d5f73f65d1e5d340d8265a4fc94629da
Shreya Paul <135226069+shreya-paul-17@users.noreply.github.com>
2025-03-15 22:29:52 +0530

    Time: 307 ms (90.14%) | Memory: 0B (100.00%) - LeetSync

--- SQL/176-second-highest-salary/second-highest-salary.sql
+++ SQL/176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,28 @@
 # Write your MySQL query statement below
 
 -- Approach 1: Using aggregated value functions
-SELECT DISTINCT MAX(salary) AS SecondHighestSalary
+-- SELECT DISTINCT MAX(salary) AS SecondHighestSalary
+-- FROM Employee a
+-- WHERE Salary < (SELECT MAX(salary) FROM Employee b WHERE b.salary > a.salary);
+
+-- Approach 2: Using Window function
+-- WITH CTE AS (
+--     SELECT Salary, RANK() OVER (ORDER BY Salary DESC) AS RANK_desc
+--     FROM Employee
+-- )
+-- SELECT MAX(salary) AS SecondHighestSalary
+-- FROM CTE
+-- WHERE RANK_desc = 2;
+
+-- Approach 3: Using Order By value
+-- SELECT IFNULL((SELECT DISTINCT Salary
+-- FROM Employee
+-- ORDER BY Salary DESC
+-- LIMIT 1,1), NULL) AS SecondHighestSalary;
+
+-- Approach 4:
+SELECT MAX(a.Salary) AS SecondHighestSalary
 FROM Employee a
-WHERE Salary < (SELECT MAX(salary) FROM Employee b WHERE b.salary > a.salary);
+JOIN Employee b
+ON a.Salary < b.Salary;
+
