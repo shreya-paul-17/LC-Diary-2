ed12eac2f38113582fc6944c2ede218cd793a688
Shreya Paul <135226069+shreya-paul-17@users.noreply.github.com>
2025-03-15 21:31:09 +0530

    Time: 610 ms (64.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+-- Approach 1: RegEx
+-- SELECT * FROM patients WHERE conditions REGEXP '\\bDIAB1';
+
+-- Approach 2: Basic
+-- SELECT patient_id, patient_name, conditions
+-- FROM patients
+-- WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
+
+-- Approach 3: Uding LOCATE function
+SELECT * 
+FROM patients
+WHERE LOCATE('DIAB1', conditions) = 1
+OR LOCATE(' DIAB1', conditions) != 0;
