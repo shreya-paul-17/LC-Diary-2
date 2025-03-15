736a129d041c9d9f77f84b71845c8bc228229ba8
Shreya Paul <135226069+shreya-paul-17@users.noreply.github.com>
2025-03-15 21:31:32 +0530

    Time: 707 ms (46.15%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1670-patients-with-a-condition/patients-with-a-condition.sql
+++ SQL/1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -4,12 +4,12 @@
 -- SELECT * FROM patients WHERE conditions REGEXP '\\bDIAB1';
 
 -- Approach 2: Basic
--- SELECT patient_id, patient_name, conditions
--- FROM patients
--- WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
-
--- Approach 3: Uding LOCATE function
-SELECT * 
+SELECT patient_id, patient_name, conditions
 FROM patients
-WHERE LOCATE('DIAB1', conditions) = 1
-OR LOCATE(' DIAB1', conditions) != 0;
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
+
+-- Approach 3: Using LOCATE function
+-- SELECT * 
+-- FROM patients
+-- WHERE LOCATE('DIAB1', conditions) = 1
+-- OR LOCATE(' DIAB1', conditions) != 0;
