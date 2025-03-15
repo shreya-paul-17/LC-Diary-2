d5e2463a3099de1718bf1b18e1c4bf5251f6296b
Shreya Paul <135226069+shreya-paul-17@users.noreply.github.com>
2025-03-15 20:36:15 +0530

    Time: 1191 ms (33.28%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,28 @@
+# Write your MySQL query statement below
+
+-- 1. Approach-1
+-- SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+-- FROM (
+--     SELECT tiv_2016,
+--         COUNT(*) OVER(PARTITION BY tiv_2015) AS tiv_2015_count,
+--         COUNT(*) OVER(PARTITION BY lat, lon) AS city_count
+--     FROM Insurance
+-- ) AS InsuranceWithCounts
+-- WHERE tiv_2015_count > 1
+-- AND city_count = 1;
+
+-- 2. Approach-2
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY 1
+    HAVING COUNT(*) > 1
+) AND CONCAT (lat, lon) IN
+(
+    SELECT CONCAT (lat, lon)
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+);
